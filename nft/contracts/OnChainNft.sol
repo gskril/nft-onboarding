930c6eda361a35610c02b043e58999f73d959970
Greg <gskril@gmail.com>
2022-02-28 18:30:41 -0500

    Add minterAddress and blockTime

--- nft/contracts/OnChainNft.sol
+++ nft/contracts/OnChainNft.sol
@@ -9,29 +9,34 @@ import "./Base64.sol";
 contract OnChainNft is ERC721Enumerable, Ownable {
   using Strings for uint256;
 
-  struct metadata {
-    string name;
-    string description;
-    string minter;
-  }
-
-  mapping (uint256 => metadata) private meta;
-
   uint256 public cost = 0.05 ether;
   bool public paused = false;
 
   constructor() ERC721("On Chain NFT", "OCN") {}
 
-  function mint(string memory _minter) public payable {
+  struct metadata {
+    string name;
+    string description;
+    string minterName;
+    string minterAddress;
+    string blockTime;
+  }
+
+  mapping (uint256 => metadata) private meta;
+
+  function mint(string memory _minterName) public payable {
     uint256 supply = totalSupply();
     require(!paused);
-    require(bytes(_minter).length > 2);
+    require(bytes(_minterName).length > 2);
     require(balanceOf(msg.sender) == 0, "Each address may only own one square");
 
     metadata memory newMetadata = metadata(
       string(abi.encodePacked('OCN #', uint256(supply+1).toString())),
       "This is our cool on chain NFT",
-      _minter
+      _minterName,
+      // TODO: slice string to 0x000...0000
+      string(abi.encodePacked('0x', toAsciiString(msg.sender))),
+      block.timestamp.toString()
     );
 
     if (msg.sender != owner()) {
@@ -42,18 +47,24 @@ contract OnChainNft is ERC721Enumerable, Ownable {
     _safeMint(msg.sender, supply + 1);
   }
 
-  function buildImage(uint tokenId) private view returns(string memory) {
+  function buildImage(uint tokenId) public view returns(string memory) {
+    require(
+      _exists(tokenId),
+      "ERC721Metadata: URI query for nonexistent token"
+    );
+
     metadata memory currentMetadata = meta[tokenId];
 
-    return Base64.encode(bytes(abi.encodePacked(
+    return string(abi.encodePacked(
+      'data:image/svg+xml;base64,',Base64.encode(bytes(abi.encodePacked(
       '<svg width="1200" height="1200" xmlns="http://www.w3.org/2000/svg">',
         '<rect width="1200" height="1200" fill="url(#background_gradient_blue)"/>',
         '<g filter="url(#tweet_shadow)"><rect x="120" y="393" width="960" height="415" rx="24" fill="white"/></g>',
         '<circle cx="221" cy="497" r="46" fill="url(#pfp_gradient_blue)"/>',
-        '<text x="287" y="486" font-size="47px" fill="black">',currentMetadata.minter,'</text>',
-        '<text x="287" y="531" font-size="25.5px" fill="#989898">','0x179...9285','</text>',
+        '<text x="287" y="486" font-size="47px" fill="black">',currentMetadata.minterName,'</text>',
+        '<text x="287" y="531" font-size="25.5px" fill="#989898">',currentMetadata.minterAddress,'</text>',
         '<text x="175" y="649" font-size="49px" fill="#000000">just setting up my wallet</text>',
-        '<text x="175" y="764" font-size="26px" fill="#7a7a7a"><tspan xml:space="preserve">','Jan 13, 2022','</tspan></text>',
+        '<text x="175" y="764" font-size="26px" fill="#7a7a7a"><tspan xml:space="preserve">',currentMetadata.blockTime,'</tspan></text>',
         '<defs>',
           '<linearGradient id="background_gradient_blue" x1="0" y1="0" x2="1200" y2="1200" gradientUnits="userSpaceOnUse"><stop stop-color="#2E80DF"/><stop offset="1" stop-color="#7CB8FF"/></linearGradient>',
           '<linearGradient id="pfp_gradient_blue" x1="175" y1="451" x2="267" y2="543" gradientUnits="userSpaceOnUse"><stop stop-color="#2E80DF"/><stop offset="1" stop-color="#7CB8FF"/></linearGradient>',
@@ -69,7 +80,7 @@ contract OnChainNft is ERC721Enumerable, Ownable {
           '</style>',
         '</defs>',
       '</svg>'
-    )));
+    )))));
   }
 
   function tokenURI(uint256 tokenId)
@@ -79,12 +90,12 @@ contract OnChainNft is ERC721Enumerable, Ownable {
     override
     returns (string memory)
   {
+    metadata memory currentMetadata = meta[tokenId];
+
     require(
       _exists(tokenId),
       "ERC721Metadata: URI query for nonexistent token"
     );
-
-    metadata memory currentMetadata = meta[tokenId];
 
     return string(abi.encodePacked(
       'data:application/json;base64,', Base64.encode(bytes(abi.encodePacked(
@@ -93,11 +104,27 @@ contract OnChainNft is ERC721Enumerable, Ownable {
         '", "description":"',
         currentMetadata.description,
         '", "image": "',
-        'data:image/svg+xml;base64,',
         buildImage(tokenId),
         '"}'
       )))
     ));
+  }
+
+  function toAsciiString(address x) internal pure returns (string memory) {
+    bytes memory s = new bytes(40);
+    for (uint i = 0; i < 20; i++) {
+        bytes1 b = bytes1(uint8(uint(uint160(x)) / (2**(8*(19 - i)))));
+        bytes1 hi = bytes1(uint8(b) / 16);
+        bytes1 lo = bytes1(uint8(b) - 16 * uint8(hi));
+        s[2*i] = char(hi);
+        s[2*i+1] = char(lo);            
+    }
+    return string(s);
+  }
+
+  function char(bytes1 b) internal pure returns (bytes1 c) {
+    if (uint8(b) < 10) return bytes1(uint8(b) + 0x30);
+    else return bytes1(uint8(b) + 0x57);
   }
 
   function setCost(uint256 _newCost) public onlyOwner {
