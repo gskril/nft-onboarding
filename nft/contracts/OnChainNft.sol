afabec6945549ec98b408c6eb8fac0dc428098af
Greg <gskril@gmail.com>
2022-03-01 15:35:06 -0500

    Remove purple, add dynamic metadata

--- nft/contracts/OnChainNft.sol
+++ nft/contracts/OnChainNft.sol
@@ -26,7 +26,7 @@ contract OnChainNft is ERC721Enumerable, Ownable {
 
   mapping (uint256 => metadata) private meta;
 
-  string[] private colorOptions = ["blue", "dark", "light", "purple", "red"];
+  string[] private colorOptions = ["blue", "dark", "light", "red"];
 
   function mint(string memory _minterName, string memory _color) public payable {
     uint256 supply = totalSupply();
@@ -40,11 +40,11 @@ contract OnChainNft is ERC721Enumerable, Ownable {
     }
 
     metadata memory newMetadata = metadata(
-      string(abi.encodePacked('OCN #', uint256(supply+1).toString())),
-      "This is our cool on chain NFT",
+      string(abi.encodePacked('#', uint256(supply+1).toString())),
+      string(abi.encodePacked("On-chain NFT test by ", _minterName)),
       _minterName,
       // TODO: slice string to 0x000...0000
-      string(abi.encodePacked('0x', AddressToString.decode(msg.sender))),
+      string(abi.encodePacked("0x", AddressToString.decode(msg.sender))),
       block.timestamp.toString(),
       _color
     );
@@ -67,7 +67,7 @@ contract OnChainNft is ERC721Enumerable, Ownable {
 
     return string(abi.encodePacked(
       'data:image/svg+xml;base64,',Base64.encode(bytes(abi.encodePacked(
-      '<svg width="1200" height="1200" xmlns="http://www.w3.org/2000/svg"><rect width="1200" height="1200" fill="url(#background_gradient_',currentMetadata.color,')"/><g filter="url(#tweet_shadow)"><rect x="120" y="393" width="960" height="415" rx="24" fill="#ffffff"/></g><circle cx="221" cy="497" r="46" fill="url(#pfp_gradient_',currentMetadata.color,')"/><text x="287" y="486" font-size="47px" fill="black">',currentMetadata.minterName,'</text><text x="287" y="531" font-size="25.5px" fill="#989898">',currentMetadata.minterAddress,'</text><text x="175" y="649" font-size="49px" fill="#000000">just setting up my wallet</text><text x="175" y="764" font-size="26px" fill="#989898"><tspan xml:space="preserve">Minted at ',currentMetadata.blockTime,' unix time</tspan></text><defs><linearGradient id="background_gradient_blue" x1="0" y1="0" x2="1200" y2="1200" gradientUnits="userSpaceOnUse"><stop stop-color="#2E80DF"/><stop offset="1" stop-color="#7CB8FF"/></linearGradient><linearGradient id="pfp_gradient_blue" x1="175" y1="451" x2="267" y2="543" gradientUnits="userSpaceOnUse"><stop stop-color="#2E80DF"/><stop offset="1" stop-color="#7CB8FF"/></linearGradient><linearGradient id="background_gradient_purple" x1="0" y1="0" x2="1200" y2="1200" gradientUnits="userSpaceOnUse"><stop stop-color="#8854bb"/><stop offset="1" stop-color="#492073"/></linearGradient><linearGradient id="pfp_gradient_purple" x1="175" y1="451" x2="267" y2="543" gradientUnits="userSpaceOnUse"><stop stop-color="#8854bb"/><stop offset="1" stop-color="#492073"/></linearGradient><linearGradient id="background_gradient_red" x1="0" y1="0" x2="1200" y2="1200" gradientUnits="userSpaceOnUse"><stop stop-color="#E46060"/><stop offset="1" stop-color="#FFB1B1"/></linearGradient><linearGradient id="pfp_gradient_red" x1="175" y1="451" x2="267" y2="543" gradientUnits="userSpaceOnUse"><stop stop-color="#E46060"/><stop offset="1" stop-color="#FFB1B1"/></linearGradient><linearGradient id="background_gradient_light" x1="0" y1="0" x2="1200" y2="1200" gradientUnits="userSpaceOnUse"><stop stop-color="#FCFCFC"/><stop offset="1" stop-color="#C8E5FF"/></linearGradient><linearGradient id="pfp_gradient_light" x1="175" y1="451" x2="267" y2="543" gradientUnits="userSpaceOnUse"><stop stop-color="#FCFCFC"/><stop offset="1" stop-color="#C8E5FF"/></linearGradient><linearGradient id="background_gradient_dark" x1="0" y1="0" x2="1200" y2="1200" gradientUnits="userSpaceOnUse"><stop stop-color="#000000"/><stop offset="1" stop-color="#333333"/></linearGradient><linearGradient id="pfp_gradient_dark" x1="175" y1="451" x2="267" y2="543" gradientUnits="userSpaceOnUse"><stop stop-color="#000000"/><stop offset="1" stop-color="#606060"/></linearGradient><filter id="tweet_shadow" x="116" y="393" width="968" height="423" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/><feOffset dy="4"/><feGaussianBlur stdDeviation="2"/><feComposite in2="hardAlpha" operator="out"/><feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/><feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_53_11"/><feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_53_11" result="shape"/></filter><style>text {font-family: sans-serif;font-weight: bold;font-style: normal;line-height: 34px;}</style></defs></svg>'
+      '<svg width="1200" height="1200" xmlns="http://www.w3.org/2000/svg"><rect width="1200" height="1200" fill="url(#background_gradient_',currentMetadata.color,')"/><g filter="url(#tweet_shadow)"><rect x="120" y="393" width="960" height="415" rx="24" fill="#ffffff"/></g><circle cx="221" cy="497" r="46" fill="url(#pfp_gradient_',currentMetadata.color,')"/><text x="287" y="486" font-size="47px" fill="black">',currentMetadata.minterName,'</text><text x="287" y="531" font-size="25.5px" fill="#989898">',currentMetadata.minterAddress,'</text><text x="175" y="649" font-size="49px" fill="#000000">just setting up my wallet</text><text x="175" y="764" font-size="26px" fill="#989898"><tspan xml:space="preserve">Minted at ',currentMetadata.blockTime,' unix time</tspan></text><defs><linearGradient id="background_gradient_blue" x1="0" y1="0" x2="1200" y2="1200" gradientUnits="userSpaceOnUse"><stop stop-color="#2E80DF"/><stop offset="1" stop-color="#7CB8FF"/></linearGradient><linearGradient id="pfp_gradient_blue" x1="175" y1="451" x2="267" y2="543" gradientUnits="userSpaceOnUse"><stop stop-color="#2E80DF"/><stop offset="1" stop-color="#7CB8FF"/></linearGradient><linearGradient id="background_gradient_red" x1="0" y1="0" x2="1200" y2="1200" gradientUnits="userSpaceOnUse"><stop stop-color="#E46060"/><stop offset="1" stop-color="#FFB1B1"/></linearGradient><linearGradient id="pfp_gradient_red" x1="175" y1="451" x2="267" y2="543" gradientUnits="userSpaceOnUse"><stop stop-color="#E46060"/><stop offset="1" stop-color="#FFB1B1"/></linearGradient><linearGradient id="background_gradient_light" x1="0" y1="0" x2="1200" y2="1200" gradientUnits="userSpaceOnUse"><stop stop-color="#FCFCFC"/><stop offset="1" stop-color="#C8E5FF"/></linearGradient><linearGradient id="pfp_gradient_light" x1="175" y1="451" x2="267" y2="543" gradientUnits="userSpaceOnUse"><stop stop-color="#FCFCFC"/><stop offset="1" stop-color="#C8E5FF"/></linearGradient><linearGradient id="background_gradient_dark" x1="0" y1="0" x2="1200" y2="1200" gradientUnits="userSpaceOnUse"><stop stop-color="#000000"/><stop offset="1" stop-color="#333333"/></linearGradient><linearGradient id="pfp_gradient_dark" x1="175" y1="451" x2="267" y2="543" gradientUnits="userSpaceOnUse"><stop stop-color="#000000"/><stop offset="1" stop-color="#606060"/></linearGradient><filter id="tweet_shadow" x="116" y="393" width="968" height="423" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/><feOffset dy="4"/><feGaussianBlur stdDeviation="2"/><feComposite in2="hardAlpha" operator="out"/><feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/><feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_53_11"/><feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_53_11" result="shape"/></filter><style>text {font-family: sans-serif;font-weight: bold;font-style: normal;line-height: 34px;}</style></defs></svg>'
     )))));
   }
 
@@ -88,11 +88,14 @@ contract OnChainNft is ERC721Enumerable, Ownable {
     return string(abi.encodePacked(
       'data:application/json;base64,', Base64.encode(bytes(abi.encodePacked(
         '{"name":"',
-        currentMetadata.name,
+          currentMetadata.name,
         '", "description":"',
-        currentMetadata.description,
-        '", "image": "',
-        buildImage(tokenId),
+          currentMetadata.description,
+        '", "attributes": [',
+          '{"trait_type": "Color", "value": "',currentMetadata.color,'"},',
+          '{"trait_type": "Minted", "value": "',currentMetadata.blockTime,'", "display_type": "date"}',
+        '], "image": "',
+          buildImage(tokenId),
         '"}'
       )))
     ));
--- nft/test/mint.js
+++ nft/test/mint.js
@@ -4,15 +4,15 @@ expect = chai.expect;
 const { ethers } = require("hardhat");
 
 describe("Contract", function () {
-  it("Should return the Base64 of the SVG", async function () {
+  it("Should return the Base64 of the JSON metadata", async function () {
     const Contract = await ethers.getContractFactory("OnChainNft");
     const contract = await Contract.deploy();
     await contract.deployed();
 
     await contract.mint('Greg', 'light');
 
-    const image = await contract.buildImage(1);
-    expect(image).to.startsWith('data:image/svg+xml;base64');
+    const image = await contract.tokenURI(1);
+    expect(image).to.startsWith('data:application/json;base64');
     console.log(image);
   });
 });
